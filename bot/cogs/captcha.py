1e5ccd8b3e4d91753dbdf3bd1f93294c87f5ec45
37dev <markthedev37@gmail.com>
2022-01-04 00:02:48 -0300

    Update captcha setup and deletion process and captcha channel permissions

--- bot/cogs/captcha.py
+++ bot/cogs/captcha.py
@@ -82,19 +82,18 @@ class CaptchaCog(commands.Cog, name="Setup Captcha command"):
 
                         perms = captcha_channel.overwrites_for(temporary_role)
                         perms.read_messages = True
-                        perms.send_messages = True
+                        perms.send_messages = False
                         await captcha_channel.set_permissions(temporary_role, overwrite=perms)
 
                         perms = captcha_channel.overwrites_for(ctx.guild.default_role)
                         perms.read_messages = False
                         await captcha_channel.set_permissions(ctx.guild.default_role, overwrite=perms)
 
-                        await captcha_channel.edit(slowmode_delay=5)
-
                         # Create captcha images channel
                         captcha_images_channel = await ctx.guild.create_text_channel('captcha-images-channel')
                         perms = captcha_images_channel.overwrites_for(ctx.guild.default_role)
                         perms.read_messages = False
+                        perms.send_messages = False
                         await captcha_images_channel.set_permissions(ctx.guild.default_role, overwrite=perms)
 
                         # Create log channel
@@ -154,50 +153,80 @@ class CaptchaCog(commands.Cog, name="Setup Captcha command"):
                 await ctx.channel.send(embed=embed)
 
         else:
-            loading = await ctx.channel.send(
-                self.bot.translate.message(ctx.guild.id, "captcha", "DELETION_OF_THE_CAPTCHA_PROTECTION"))
-            config = get_config(ctx.guild.id)
-            config["captcha"] = False
-
-            # Delete all
-            not_deleted = []
-            try:
-                temporary_role = get(ctx.guild.roles, id=config["temporary_role"])
-                await temporary_role.delete()
-            except (HTTPException, AttributeError, KeyError):
-                not_deleted.append("temporary_role")
-            try:
-                captcha_channel = self.bot.get_channel(config["captcha_channel"])
-                await captcha_channel.delete()
-            except (HTTPException, AttributeError, KeyError):
-                not_deleted.append("captcha_channel")
-
-            try:
-                captcha_images_channel = self.bot.get_channel(config["captcha_images_channel"])
-                await captcha_images_channel.delete()
-            except (HTTPException, AttributeError, KeyError):
-                not_deleted.append("captcha_images_channel")
-
-            # Add modifications
-            config["captcha_channel"] = False
-
-            # Edit configuration.json
-            update_config(ctx.guild.id, config)
-
-            await loading.delete()
             embed = nextcord.Embed(
-                title=self.bot.translate.message(ctx.guild.id, "captcha", "CAPTCHA_WAS_DELETED_WITH_SUCCESS"),
-                description=self.bot.translate.message(ctx.guild.id, "captcha",
-                                                       "CAPTCHA_WAS_DELETED_WITH_SUCCESS_DESCRIPTION"),
-                color=0x2fa737)  # Green
-            await ctx.channel.send(embed=embed)
-            if len(not_deleted) > 0:
-                errors = ", ".join(not_deleted)
-                embed = nextcord.Embed(
-                    title=self.bot.translate.message(ctx.guild.id, "captcha", "CAPTCHA_DELETION_ERROR"),
-                    description=self.bot.translate.message(ctx.guild.id, "captcha",
-                                                           "CAPTCHA_DELETION_ERROR_DESCRIPTION").format(
-                        errors), color=0xe00000)  # Red
+                title=self.bot.translate.message(
+                    ctx.guild.id,
+                    "captcha",
+                    "DO_YOU_WANT_TO_DELETE_THE_CAPTCHA_PROTECTION"
+                ),
+                description=self.bot.translate.message(
+                    ctx.guild.id,
+                    "captcha",
+                    "DO_YOU_WANT_TO_DELETE_THE_CAPTCHA_PROTECTION_DESCRIPTION"
+                ),
+                color=0xff0000
+            )
+            confirmation_view = ConfirmationView()
+            await ctx.channel.send(embed=embed, view=confirmation_view)
+            await confirmation_view.wait()
+
+            try:
+                if not confirmation_view.confirmed:
+                    await ctx.channel.send(self.bot.translate.message(ctx.guild.id, "captcha", "DELETE_ABANDONED"))
+                else:
+                    loading = await ctx.channel.send(
+                        self.bot.translate.message(ctx.guild.id, "captcha", "DELETION_OF_THE_CAPTCHA_PROTECTION"))
+                    config = get_config(ctx.guild.id)
+                    config["captcha"] = False
+
+                    # Delete all
+                    not_deleted = []
+                    try:
+                        temporary_role = get(ctx.guild.roles, id=config["temporary_role"])
+                        await temporary_role.delete()
+                    except (HTTPException, AttributeError, KeyError):
+                        not_deleted.append("temporary_role")
+                    try:
+                        captcha_channel = self.bot.get_channel(config["captcha_channel"])
+                        await captcha_channel.delete()
+                    except (HTTPException, AttributeError, KeyError):
+                        not_deleted.append("captcha_channel")
+
+                    try:
+                        captcha_images_channel = self.bot.get_channel(config["captcha_images_channel"])
+                        await captcha_images_channel.delete()
+                    except (HTTPException, AttributeError, KeyError):
+                        not_deleted.append("captcha_images_channel")
+
+                    # Add modifications
+                    config["captcha_channel"] = False
+
+                    # Edit configuration.json
+                    update_config(ctx.guild.id, config)
+
+                    await loading.delete()
+                    embed = nextcord.Embed(
+                        title=self.bot.translate.message(ctx.guild.id, "captcha", "CAPTCHA_WAS_DELETED_WITH_SUCCESS"),
+                        description=self.bot.translate.message(ctx.guild.id, "captcha",
+                                                               "CAPTCHA_WAS_DELETED_WITH_SUCCESS_DESCRIPTION"),
+                        color=0x2fa737)  # Green
+                    await ctx.channel.send(embed=embed)
+                    if len(not_deleted) > 0:
+                        errors = ", ".join(not_deleted)
+                        embed = nextcord.Embed(
+                            title=self.bot.translate.message(ctx.guild.id, "captcha", "CAPTCHA_DELETION_ERROR"),
+                            description=self.bot.translate.message(ctx.guild.id, "captcha",
+                                                                   "CAPTCHA_DELETION_ERROR_DESCRIPTION").format(
+                                errors), color=0xe00000)  # Red
+                        await ctx.channel.send(embed=embed)
+
+            except asyncio.TimeoutError:
+                embed = nextcord.Embed(title=self.bot.translate.message(ctx.guild.id, "captcha", "TIME_IS_OUT"),
+                                       description=self.bot.translate.message(
+                                           ctx.guild.id, "setup",
+                                           "USER_HAS_EXCEEDED_THE_RESPONSE_TIME").format(
+                                           ctx.author.mention
+                                       ), color=0xff0000)
                 await ctx.channel.send(embed=embed)
 
 
